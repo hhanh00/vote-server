a469dfc568f9e1ec76f1938dc03031cda0c2be35
hhanh00 <hanh425@gmail.com>
2024-12-30 23:48:07 +1000

    feat: update dependencies (orchard & zcash-vote) (#2)

--- src/db.rs
+++ src/db.rs
@@ -2,7 +2,7 @@ use anyhow::{anyhow, Result};
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::{params, Connection, OptionalExtension as _};
-use zcash_vote::{proof::ValidationResult, Election, Hash};
+use zcash_vote::{ballot::Ballot, Election, Hash};
 
 pub const DB_FILE: &str = "vote.db";
 pub const REFDATA_FILE: &str = "refdata.db";
@@ -30,42 +30,43 @@ pub fn create_db(connection: &Connection) -> Result<()> {
 
 pub fn store_ballot(
     election: &Election,
-    ballot: &ValidationResult,
+    ballot: &Ballot,
     ballot_bytes: &[u8],
     pool: &Pool<SqliteConnectionManager>,
-) -> Result<Hash> {
+) -> Result<()> {
     let mut connection = pool.get().unwrap();
     let db_tx = connection.transaction()?;
-    let candidate = u32::from_le_bytes(ballot.candidate.clone().try_into().unwrap());
-    db_tx.execute(
-        "INSERT INTO votes(id_election, sig_hash, amount, candidate, data)
-    VALUES (?1, ?2, ?3, ?4, ?5)",
-        params![
-            election.id,
-            &ballot.sig_hash,
-            ballot.amount,
-            candidate,
-            ballot_bytes
-        ],
-    )?;
-    let id_vote = db_tx.last_insert_rowid();
-    for nf in ballot.nfs.iter() {
-        let dup = db_tx
-            .query_row("SELECT 1 FROM nfs WHERE hash = ?1", [nf], |_r| Ok(()))
-            .optional()?;
-        if dup.is_some() {
-            anyhow::bail!("Duplicate vote");
-        }
-        db_tx.execute(
-            "INSERT INTO nfs(vote, hash) VALUES (?1, ?2)",
-            params![id_vote, nf],
-        )?;
-    }
-    db_tx.commit()?;
-    tracing::debug!("{:?}", ballot);
-    tracing::info!("Validated & Stored");
+    // let candidate = u32::from_le_bytes(ballot.candidate.clone().try_into().unwrap());
+    // db_tx.execute(
+    //     "INSERT INTO votes(id_election, sig_hash, amount, candidate, data)
+    // VALUES (?1, ?2, ?3, ?4, ?5)",
+    //     params![
+    //         election.id,
+    //         &ballot.data.sighash()?,
+    //         ballot.amount,
+    //         candidate,
+    //         ballot_bytes
+    //     ],
+    // )?;
+    // let id_vote = db_tx.last_insert_rowid();
+    // for nf in ballot.nfs.iter() {
+    //     let dup = db_tx
+    //         .query_row("SELECT 1 FROM nfs WHERE hash = ?1", [nf], |_r| Ok(()))
+    //         .optional()?;
+    //     if dup.is_some() {
+    //         anyhow::bail!("Duplicate vote");
+    //     }
+    //     db_tx.execute(
+    //         "INSERT INTO nfs(vote, hash) VALUES (?1, ?2)",
+    //         params![id_vote, nf],
+    //     )?;
+    // }
+    // db_tx.commit()?;
+    // tracing::debug!("{:?}", ballot);
+    // tracing::info!("Validated & Stored");
 
-    Ok(ballot.sig_hash)
+    // Ok(ballot.sig_hash)
+    Ok(())
 }
 
 pub fn get_ballot_bytes(hash: &[u8], pool: &Pool<SqliteConnectionManager>) -> Result<Vec<u8>> {
